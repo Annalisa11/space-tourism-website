6b38122ea52da97bc0cfd743b8bb984e5166b993
Annalisa Comin <comin@cip.ifi.lmu.de>
2024-06-22 22:28:02 +0200

    error page

--- src/pages/NoMatch.tsx
+++ src/pages/NoMatch.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const NoMatch = () => {
-  return <div className="">Nothing to see here</div>;
+  return (
+    <div className="mt-32 flex w-full flex-col items-center justify-center gap-8">
+      <div className="font-dosis text-6xl uppercase text-white">Oh no..</div>
+      <div className="font-dosis text-3xl uppercase text-white">
+        Nothing to see here
+      </div>
+    </div>
+  );
 };
 
 export default NoMatch;
